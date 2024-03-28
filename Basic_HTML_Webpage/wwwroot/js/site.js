468709b610dfc0c917b1f0e44e683b6058c57c7a
Elizabeth <itsybitsy1121@outlook.com>
2024-03-27 18:17:06 -0800

    made a Basic_HTML_Webpage project

---
+++ Basic_HTML_Webpage/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function changeColor(element) 
+{
+    element.style.color = "blue"; // Change to your desired color
+}
+
+// Function to reset the color when mouse moves away from the text
+function resetColor(element) 
+{
+    element.style.color = "#333"; // Reset to the original color
+}
